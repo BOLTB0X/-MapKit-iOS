85aa01af701cef665d0559e4a4d4815c257ce1c8
B0X <83914919+BOLTB0X@users.noreply.github.com>
2024-01-15 16:22:06 +0900

    commit
    내 위치, 내위치로 되돌아오기

--- MapBoxSwiftUI/MapBoxSwiftUI/ContentView.swift
+++ MapBoxSwiftUI/MapBoxSwiftUI/ContentView.swift
@@ -8,39 +8,29 @@
 import SwiftUI
 @_spi(Experimental) import MapboxMaps
 
-struct MarkPoints: Identifiable {
-    let id = UUID()
-    let pos: CLLocationCoordinate2D
-    let imgString: String
-    
-    init(pos: CLLocationCoordinate2D, imgString: String) {
-        self.pos = pos
-        self.imgString = imgString
-    }
-}
-
+// MARK: - ContentView
 struct ContentView: View {
+    // MARK: 프로퍼티s
     @Environment(\.colorScheme) var colorScheme
     @State var viewport: Viewport = .styleDefault
     @State var markPointsArr : [MarkPoints] = [
-        MarkPoints(pos: CLLocationCoordinate2D(latitude: 37.627647, longitude: 127.069929), imgString: "house"),
-        MarkPoints(pos: CLLocationCoordinate2D(latitude: 37.625773, longitude: 127.072940), imgString: "lightbulb.fill"),
-        MarkPoints(pos: CLLocationCoordinate2D(latitude: 37.631905, longitude: 127.077548), imgString: "graduationcap.fill")]
-    
-    let apart = CLLocationCoordinate2D(latitude: 37.627647, longitude: 127.069929)
-    let gongneung = CLLocationCoordinate2D(latitude: 37.625773, longitude: 127.072940)
-    let UniversityofTechn = CLLocationCoordinate2D(latitude: 37.631905, longitude: 127.077548)
-    
+        MarkPoints(pos: .gongneungStation, imgString: "lightbulb.fill"),
+        MarkPoints(pos: .samsungServiceCenter, imgString: "lightbulb.fill"),
+        MarkPoints(pos: .starbucks, imgString: "lightbulb.fill"),
+        MarkPoints(pos: .universityofTechn, imgString: "lightbulb.fill")
+    ]
+        
     let polygon = Polygon(center: CLLocationCoordinate2D(latitude: 37.625773, longitude: 127.072940), radius: 1000, vertices: 5)
     
-    
+    // MARK: - View
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
             Map(viewport: $viewport) {
                 // 내 위치
                 Puck2D(bearing: .heading)
-                    .showsAccuracyRing(true)
+                    .showsAccuracyRing(false)
                 
+                // foreach
                 ForEvery(markPointsArr) { arr in
                     MapViewAnnotation(coordinate: arr.pos) {
                         Image(systemName: arr.imgString)
@@ -51,21 +41,6 @@ struct ContentView: View {
                         ViewAnnotationAnchorConfig(anchor: .bottom)
                     ])
                 } // ForEvery
-                
-                MapViewAnnotation(layerId: "route") {
-                    Text("55 min")
-                }
-                
-                // Polygon 추가
-                PolygonAnnotation(polygon: polygon)
-                    .fillColor(StyleColor(.systemGreen))
-                    .fillOpacity(0.5)
-                    .onTapGesture {
-                        print("Polygon is tapped")
-                    }
-                    .onLongPressGesture {
-                        print("Polygon is long-pressed")
-                    }
             } // Map
             
             .mapStyle(.standard(lightPreset: colorScheme == .light ? .day : .dusk))
@@ -73,15 +48,21 @@ struct ContentView: View {
             
             HStack(alignment: .center, spacing: 0) {
                 Button("Overview route") {
-                    // LineString 위치로 카메라 이동
                     withViewportAnimation(.easeIn(duration: 1)) {
-                        viewport = .overview(geometry: LineString([apart, gongneung]))
+                        viewport = .overview(geometry: LineString([.tmpRoadStartPoints, .tmpRoadEndPoints]))
                     }
                 }
+                
                 Spacer()
+                
+                Button("Record") {
+                    
+                }
+                
+                Spacer()
+                
                 Button("Locate the user") {
-                    // viewport 위치 업데이트
-                    withViewportAnimation {
+                    withViewportAnimation {  // viewport 위치 업데이트
                         viewport = .followPuck(zoom: 20, pitch: 20)
                     }
                 }
@@ -89,10 +70,11 @@ struct ContentView: View {
             .padding(.horizontal)
         } // VStack
         .onAppear {
-            viewport = .camera(center: apart, zoom: 14, bearing: 0, pitch: 0)
+            viewport = .followPuck(zoom: 17, pitch: 17)
         }
     } // body
 }
+
 
 #Preview {
     ContentView()
